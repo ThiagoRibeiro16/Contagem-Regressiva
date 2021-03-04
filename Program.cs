956966d0175688d1b448729f54c7ec6eafeec4f3
Thiago Ribeiro <thiago.escola321@gmail.com>
2021-03-04 18:54:13 -0300

    Finalização do projeto de contagem regressiva

---
+++ Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace Contagem_Regressiva
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            Console.Clear();
+            Console.WriteLine();
+
+            Console.ReadKey();
+            Console.ForegroundColor= ConsoleColor.Blue;
+            
+            Console.WriteLine("---------------");
+            Console.WriteLine("/ Preparar... /");
+            Console.WriteLine("---------------");
+           
+            Console.ReadKey();
+            Console.ForegroundColor= ConsoleColor.Green;
+            
+            Console.WriteLine("--------------");
+            Console.WriteLine("/ Apontar... / ");
+            Console.WriteLine("-------------");
+           
+            Console.ReadKey();
+            Console.ForegroundColor= ConsoleColor.Red;
+           
+            Console.WriteLine("---------");
+            Console.WriteLine("/ Fogo! /");
+            Console.WriteLine("--------");
+           
+            Console.ReadKey();
+            Console.ForegroundColor= ConsoleColor.White;
+        }
+    }
+}
